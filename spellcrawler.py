73ce3233a4bd90801b495917f8b422f5d1a0b7c5
Caleb Van Tassel <calebvantassel@gmail.com>
2019-12-04 15:39:34 -0600

    Implemented checking via suggestqueries

--- spellcrawler.py
+++ spellcrawler.py
@@ -2,11 +2,24 @@ import scrapy
 from spellchecker import SpellChecker
 import html2text    
 import sys
+import requests
 import re
+
+# apiKey = 'AIzaSyDW3q3O9vy3c8tFohu7FsoQtLUiv4ZQSM8'
+# cx = '014357231247660817202:8nrqpcejysl'
+
+# apiRequestURL = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey
+#             + '&cx=' + cx
+#             + '&q=' + word
+
+
+
+  
 
 class SpellCheckerSpider(scrapy.Spider):
     name = "spellChecker"
-    start_urls = ['https://www.space.com/ad-astra-film-review.html']
+    # start_urls = ['https://www.space.com/ad-astra-film-review.html']
+    start_urls = ['https://en.wikipedia.org/wiki/Commonly_misspelled_English_words']
     
     def parse(self, response):
 
@@ -25,14 +38,25 @@ class SpellCheckerSpider(scrapy.Spider):
 
         noSymbolRes = []
         for word in resWords.split():
-            word = re.sub('[^A-Za-z0-9]+', ' ', word)
+            word = re.sub('[^A-Za-z0-9]+', '', word)
             noSymbolRes.append(word)
-            #fo.write()
-        spell = SpellChecker()
-        matcher = re.compile('.*[http].*')
-        for word in spell.unknown(noSymbolRes):
-            if(not matcher.match(word)):
-                fo.write(word + "\n")
+            
+
+        requestURL = 'http://suggestqueries.google.com/complete/search?client=firefox&q=' + word
+
+        for word in noSymbolRes:
+
+            # sending get request and saving the response as response object 
+            r = requests.get(url = requestURL) 
+            
+            # extracting data in json format 
+            data = r.json() 
+            suggestion = ""
+            if (len(data[1]) > 0):
+                suggestion = data[1][0]
+
+            if (word != suggestion):
+                fo.write(word + " should be: " + suggestion)
 
         fo.close() 
 
